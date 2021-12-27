39ec367e8e6d3def559b02ff4d95961294836986
Tsuna <beatles1964@yandex.ru>
2021-12-27 13:03:14 +0300

    Инициализация проекта

---
+++ src/views/About.vue
@@ -0,0 +1,15 @@
+<template>
+  <div>
+    <line-charts></line-charts>
+  </div>
+</template>
+
+<script>
+import LineCharts from '../components/LineCharts'
+
+export default {
+  components: {
+    LineCharts
+  }
+}
+</script>
